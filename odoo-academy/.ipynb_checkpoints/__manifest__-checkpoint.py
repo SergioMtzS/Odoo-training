94420d322b151d8ebe445c82e75ddfc34b560d92
SergioMtzS <sergiopsc2@gmail.com>
2023-01-23 15:32:07 +0000

    Acadeny Day 1

---
+++ odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app""",
+    
+    'description': """
+        Academy trainnig:
+        -Courses
+        -Sessions
+        -Attendees
+        """,
+    
+    'author': 'Sergio',
+    'category': 'Training',
+    'version': '0.1',
+    'depends': ['base'],
+    'data':[
+        
+        ],
+    'demo': [
+        ],
+}
