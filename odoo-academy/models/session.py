0ed44ed76dc238a039058dd6503332153d8a872a
SergioMtzS <sergiopsc2@gmail.com>
2023-02-02 01:56:53 +0000

    added wizard test

--- odoo-academy/models/session.py
+++ odoo-academy/models/session.py
@@ -1,5 +1,8 @@
+# .*. coding: utf-8 -*-
+
 from odoo import models, fields, api
 from datetime import timedelta
+
 class Session(models.Model):
     _name = 'academy.session'
     _description = 'Session Info'
@@ -14,24 +17,36 @@ class Session(models.Model):
     instructor_id = fields.Many2one(comodel_name='res.partner', string='Instructor')
 
     student_ids = fields.Many2many(comodel_name='res.partner', string='Students')
-    
+
     start_date = fields.Date(string="Start Date",
                             default=fields.Date.today)
+
     duration = fields.Integer(string='Session Days',
                              default=1)
+
     end_date = fields.Date(string='End Date',
                           compute='_compute_end_date',
                           inverse='_inverse_end_date',
                           store=True)
-    
-    
+    state = fields.Selection(string='States',
+                            selection=[('draft', 'Draft'),
+                                        ('open', 'In Progress'),
+                                        ('done', 'Done'),
+                                        ('canceled', 'Canceled')],
+                                       default='draft',
+                                       required=True)
+    total_price = fields.Float(string='Total Price',
+                              related='course_id.total_price')
+
+
+
     @api.depends('start_date', 'duration')
     def _compute_end_date(self):
         for record in self:
             if not (record.start_date and record.duration):
                 record.end_date = record.start_date
             else:
-                duration = timedelta(days=record.duration -1)
+                duration = timedelta(days=record.duration)
                 record.end_date = record.start_date + duration
 
     def _inverse_end_date(self):
@@ -40,5 +55,3 @@ class Session(models.Model):
                 record.duration = (record.end_date - record.start_date).days + 1
             else:
                 continue
-    
-    
