a92c0a33212f4e63adb52a91155aa8a4fa7e6ffa
SergioMtzS <sergiopsc2@gmail.com>
2023-02-03 18:39:21 +0000

    added controllers

--- odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo-academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,7 +16,7 @@
     'author': 'Sergio',
     'category': 'Training',
     'version': '0.1',
-    'depends': ['sale_management'],
+    'depends': ['sale_management','website'],
     'data':[
         'security/academy_security.xml',
         'security/ir.model.access.csv',
@@ -25,8 +25,10 @@
         'views/session_views.xml',
         'views/sale_views_inherit.xml',
         'views/product_views_inherit.xml',
+        'views/academy_web_templates.xml',
         'wizard/sale_wizard_view.xml',
         'report/session_report_templates.xml',
+        
         ],
     'demo': [
         'demo/academy_demo.xml',
